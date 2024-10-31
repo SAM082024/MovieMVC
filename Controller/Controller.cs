b26ed51c55c7c1112252e44450e4bec9f1680dde
SAM082024 <sam082024@gmail.com>
2024-10-31 10:47:21 +0100

    add dependancies to controller and initialize a run method

--- Controller/Controller.cs
+++ Controller/Controller.cs
@@ -1,4 +1,16 @@
 public class Controller
 {
-    
+    private Movie _model;
+    private View _view;
+
+    public Controller(Movie model, View view)
+    {
+        _model = model;
+        _view = view;
+    }
+
+    public void Run()
+    {
+        _view.displayMovies();
+    }
 }
