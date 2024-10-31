0482025b5dbfcd879d53413a6c170b60652ea712
SAM082024 <sam082024@gmail.com>
2024-10-31 10:29:23 +0100

    add movie to movielist

--- Model/Model.cs
+++ Model/Model.cs
@@ -19,6 +19,7 @@ public class Movie
             new() { Title = "Prometheus", Year = 2012, Genre = "Sci-Fi, Horror, Action, Thriller", Director = "Ridley Scott"},
             new() { Title = "Alien Covenant", Year = 2017, Genre = "Sci-Fi, Horror, Action, Thriller", Director = "Ridley Scott"},
             new() { Title = "Alien", Year = 1979, Genre = "Sci-Fi, Horror, Action, Thriller", Director = "Ridley Scott"},
+            new() { Title = "Alien", Year = 1986, Genre = "Sci-Fi, Horror, Action, Thriller", Director = "James Cameron"},
         };
     }
 }
