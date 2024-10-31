29139f002ca2f602380eb8522e8a4386b66615ee
SAM082024 <sam082024@gmail.com>
2024-10-31 10:19:37 +0100

    add movie properties

--- Model/Model.cs
+++ Model/Model.cs
@@ -1,4 +1,9 @@
+using System.Dynamic;
+
 public class Model
 {
-    
+    public string? Title { get; set; } // For displaying the title of the movie
+    public int Year { get; set; } // For displaying the release year of the movie
+    public string? Genre { get; set; } // For displaying the movie genre
+    public string? Director { get; set; } // For displaying the movie director
 }
