7972b88594589bd933cc84725fc7d08b6eb79b20
SAM082024 <sam082024@gmail.com>
2024-10-31 11:19:36 +0100

    add console movie manager prompts and user input

--- Controller/Controller.cs
+++ Controller/Controller.cs
@@ -21,7 +21,44 @@ public class Controller
         
         while (isRunning)
         {
-            
+            Console.WriteLine("Welcome to Movie Manager");
+            Console.WriteLine("1. Add a movie");
+            Console.WriteLine("2. View all movies");
+            Console.WriteLine("3. Exit program");
+            Console.Write("\nChoose an alternative, 1-3: \n");
+
+            string? choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("\nWrite movie title: ");
+                    string title = Console.ReadLine() ?? string.Empty;
+
+                    Console.Write("Write movie year: ");
+                    int year = int.Parse(Console.ReadLine() ?? "0");
+
+                    Console.Write("Write movie genre: ");
+                    string genre = Console.ReadLine() ?? string.Empty;
+
+                    Console.Write("Write movie director: ");
+                    string developer = Console.ReadLine() ?? string.Empty;
+
+                    AddMovie(title, year, genre, developer);
+                    break;
+                case "2":
+                    _view.displayMovies();
+                    break;
+                
+                case "3":
+                    isRunning = false;
+                    Console.WriteLine("\nExiting program..\n");
+                    break;
+                
+                default:
+                    Console.WriteLine("\nInvalid choice, try again\n");
+                    break;
+            }
         }
     }
 }
