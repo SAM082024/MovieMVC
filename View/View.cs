1349e14f5a307062fbac878d3e39a570a01ecde3
SAM082024 <sam082024@gmail.com>
2024-10-31 10:38:40 +0100

    add method to display movies

--- View/View.cs
+++ View/View.cs
@@ -4,6 +4,19 @@ public class View
 
     public View(Movie movie)
     {
-        
+        myMovie = movie;
+    }
+
+    public void displayMovies()
+    {
+        Console.WriteLine("Movies in the model:\n");
+
+        foreach (var movie in myMovie.Movies)
+        {
+            Console.WriteLine($"Title: {movie.Title}\n");
+            Console.WriteLine($"Year: {movie.Year}\n");
+            Console.WriteLine($"Genre: {movie.Genre}\n");
+            Console.WriteLine($"Director: {movie.Director}\n");
+        }
     }
 }
