1333aec76491363ddfbfd7b5c52b7ba728a75ebe
SAM082024 <sam082024@gmail.com>
2024-10-31 10:09:52 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieMVC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Model model = new Model();
+        View view = new View();
+        Controller ctrl = new Controller();
+    }
+}
